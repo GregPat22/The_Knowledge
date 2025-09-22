732cd9a358ad76a9f61f7f1adda6a38329d8ba0d
Greg <ninopatini@icloud.com>
2025-09-22 17:18:21 +0200

    endend the loading screen text and animation

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -1,7 +1,7 @@
-import './App.css'
-import './index.css'
-import { LoadingScreen } from './components/LoadingScreen'    
-import { useState } from 'react'
+import './App.css';
+import './index.css';
+import { LoadingScreen } from './components/LoadingScreen';
+import { useState } from 'react';
 
 
 function App() {
--- main/src/components/LoadingScreen.jsx
+++ main/src/components/LoadingScreen.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const LoadingScreen = ({ onComplete }) => {
 const [text, setText] = useState("")
-const fullText ="Loading Please Wait..";
+const fullText ="Generating Knowledge...";
 
 
 useEffect(() => {
@@ -16,18 +16,18 @@ useEffect(() => {
             
             setTimeout(()=> {
                 onComplete()
-            })
+            }, 1000);
         }
     }, 100);
 return () => clearInterval(interval);
-});
+}, [onComplete] );
 
 
     return <div className="fixed inset-0 z-50 bg-none text-amber-400 flex flex-col items-center justify-center">
         
 
         <div className="mb-4 text-4xl font-mono font-bold">
-            Loading Screen <span className="animate-blink ml-1"> | </span>
+            {text} <span className="animate-blink ml-1"> | </span>
         </div>
         <div className="w-[200px] h-[2px] bg-amber-400 rounded-2xl relative overflow-hidden">
             <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px]"></div>
