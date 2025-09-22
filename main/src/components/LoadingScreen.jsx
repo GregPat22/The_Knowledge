2adac0688c775e337443382c9affc94a0fb90d43
Greg <ninopatini@icloud.com>
2025-09-22 16:35:59 +0200

    created new function and logics for the Loading Screen and a const which recall the loaded logics, new font and google API,

--- main/src/components/LoadingScreen.jsx
+++ main/src/components/LoadingScreen.jsx
@@ -1,12 +1,35 @@
-export const LoadingScreen = () => {
+import { useState } from "react";
+
+export const LoadingScreen = ({ onComplete }) => {
+const [text, setText] = useState("")
+const fullText ="Loading Please Wait..";
+
+
+useEffect(() => {
+    let index = 0;
+    const interval = setInterval(() => {
+        setText(fullText.substring(0, index));
+        index++
+
+        if (index > fullText.length) {
+            clearInterval(interval)
+            
+            setTimeout(()=> {
+                onComplete()
+            })
+        }
+    }, 100);
+return () => clearInterval(interval);
+});
 
 
     return <div className="fixed inset-0 z-50 bg-none text-amber-400 flex flex-col items-center justify-center">
+        
 
         <div className="mb-4 text-4xl font-mono font-bold">
             Loading Screen <span className="animate-blink ml-1"> | </span>
         </div>
-        <div className="w-[200px] h-[2px] bg-gray-800 rounded-2xl relative overflow-hidden">
+        <div className="w-[200px] h-[2px] bg-amber-400 rounded-2xl relative overflow-hidden">
             <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px]"></div>
         </div>
     </div>
