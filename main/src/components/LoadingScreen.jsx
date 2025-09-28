646bc803f59e6895cb52d1e7ada24ff18ea66055
Greg <ninopatini@icloud.com>
2025-09-28 12:14:04 +0200

    fixed loading bar problem, and changed colours

--- main/src/components/LoadingScreen.jsx
+++ main/src/components/LoadingScreen.jsx
@@ -25,14 +25,14 @@ return () => clearInterval(interval);
 }, [onComplete] );
 
 
-    return <div className="fixed inset-0 z-50 bg-none text-shadow-white flex flex-col items-center justify-center">
+    return <div className="fixed inset-0 z-50 bg-black opacity-80 text-gray-100 flex flex-col items-center justify-center">
         
 
         <div className="mb-4 text-4xl font-mono font-bold">
             {text} <span className="animate-blink ml-1"> | </span>
         </div>
-        <div className="w-[200px] h-[2px] bg-white rounded-2xl relative overflow-hidden">
-            <div className="w-[40%] h-full bg-black shadow-[0_0_15px]"></div>
+        <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
+            <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
         </div>
     </div>
 }
