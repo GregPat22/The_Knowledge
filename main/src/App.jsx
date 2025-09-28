748045fee2fdd825e3d5e05975c70e373c8997f9
Greg <ninopatini@icloud.com>
2025-09-28 14:34:15 +0200

    created the Navbar in the app.jsx and styled the header

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -1,22 +1,24 @@
-import './App.css';
-import './index.css';
-import { LoadingScreen } from './components/LoadingScreen';
-import { useState } from 'react';
-import { Navbar } from './components/Navbar'; 
-
+import "./App.css";
+import "./index.css";
+import { LoadingScreen } from "./components/LoadingScreen";
+import { useState } from "react";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-  const [isLoaded, setIsLoaded] = useState(false) 
+  const [isLoaded, setIsLoaded] = useState(false);
 
-  
   return (
-  <>
-    {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>} {" "}
-      <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text text-gray-100`}>
-
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text text-gray-100`}
+      >
+        <Navbar></Navbar>
       </div>
-  </>
+    </>
   );
 }
 
-export default App
+export default App;
